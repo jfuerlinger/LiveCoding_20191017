3cb7226a0d7e2f36ade47e8770084bb34546be45
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-10-17 19:56:19 +0200

    Add sample for explicit interface implementation.

---
+++ MyStack.Core/Circles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStack.Core
+{
+    interface IRoundObject
+    {
+        void Draw();
+    }
+
+    interface IGreenObject {
+        void Draw();
+    }
+    
+    class GreenCircle : IRoundObject, IGreenObject
+    {
+        void IRoundObject.Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IGreenObject.Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw()
+        {
+            ((IGreenObject)this).Draw();
+        }
+    }
+
+
+    class Program
+    {
+        public static void Main()
+        {
+            GreenCircle gc = new GreenCircle();
+            gc.Draw();
+
+            ((IGreenObject)gc).Draw();
+            ((IRoundObject)gc).Draw();
+        }
+    }
+}
